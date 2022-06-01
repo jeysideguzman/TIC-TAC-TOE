2e50a1aa495af76665b062c4daa3cd1470d917de
John Carlo De Guzman <johncarlobdeguzman@iskolarngbayan.pup.edu.ph>
2022-06-01 11:39:28 +0800

    btncodesXO

--- TicTacToe.Designer.cs
+++ TicTacToe.Designer.cs
@@ -39,82 +39,99 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.btnplay = new System.Windows.Forms.Button();
             this.btnrestart = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn1
             // 
             this.btn1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn1.Location = new System.Drawing.Point(57, 85);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(103, 98);
             this.btn1.TabIndex = 0;
             this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // btn2
             // 
             this.btn2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn2.Location = new System.Drawing.Point(166, 85);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(103, 98);
             this.btn2.TabIndex = 1;
             this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
             // btn3
             // 
             this.btn3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn3.Location = new System.Drawing.Point(275, 85);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(103, 98);
             this.btn3.TabIndex = 2;
             this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btn4
             // 
             this.btn4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn4.Location = new System.Drawing.Point(57, 189);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(103, 98);
             this.btn4.TabIndex = 3;
             this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
             // 
             // btn5
             // 
             this.btn5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn5.Location = new System.Drawing.Point(166, 189);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(103, 98);
             this.btn5.TabIndex = 4;
             this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn6
             // 
             this.btn6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn6.Location = new System.Drawing.Point(275, 189);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(103, 98);
             this.btn6.TabIndex = 5;
             this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
             // 
             // btn7
             // 
             this.btn7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn7.Location = new System.Drawing.Point(57, 293);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(103, 98);
             this.btn7.TabIndex = 6;
             this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // btn9
             // 
             this.btn9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn9.Location = new System.Drawing.Point(275, 293);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(103, 98);
             this.btn9.TabIndex = 8;
             this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // label1
             // 
@@ -131,9 +148,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(410, 111);
+            this.label3.Location = new System.Drawing.Point(425, 134);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(119, 39);
             this.label3.TabIndex = 11;
@@ -142,34 +159,22 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(632, 111);
+            this.label4.Location = new System.Drawing.Point(608, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(123, 39);
             this.label4.TabIndex = 12;
             this.label4.Text = "Player 2 ";
-            // 
-            // btnplay
-            // 
-            this.btnplay.BackColor = System.Drawing.Color.Silver;
-            this.btnplay.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnplay.ForeColor = System.Drawing.Color.Blue;
-            this.btnplay.Location = new System.Drawing.Point(444, 29);
-            this.btnplay.Name = "btnplay";
-            this.btnplay.Size = new System.Drawing.Size(116, 46);
-            this.btnplay.TabIndex = 15;
-            this.btnplay.Text = "Play";
-            this.btnplay.UseVisualStyleBackColor = false;
             // 
             // btnrestart
             // 
             this.btnrestart.BackColor = System.Drawing.Color.Silver;
             this.btnrestart.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnrestart.ForeColor = System.Drawing.Color.Blue;
-            this.btnrestart.Location = new System.Drawing.Point(593, 29);
+            this.btnrestart.Location = new System.Drawing.Point(153, 406);
             this.btnrestart.Name = "btnrestart";
-            this.btnrestart.Size = new System.Drawing.Size(116, 46);
+            this.btnrestart.Size = new System.Drawing.Size(127, 41);
             this.btnrestart.TabIndex = 16;
             this.btnrestart.Text = "Restart";
             this.btnrestart.UseVisualStyleBackColor = false;
@@ -177,21 +182,48 @@
             // btn8
             // 
             this.btn8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn8.Location = new System.Drawing.Point(166, 293);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(103, 98);
             this.btn8.TabIndex = 17;
             this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(470, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 39);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "0";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(650, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 39);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "0";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(865, 523);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 459);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btn8);
             this.Controls.Add(this.btnrestart);
-            this.Controls.Add(this.btnplay);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
@@ -207,6 +239,7 @@
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "TicTacToe";
             this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,9 +258,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnplay;
         private System.Windows.Forms.Button btnrestart;
         private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -16,5 +16,87 @@ namespace Tic_Tac_Toe
         {
             InitializeComponent();
         }
+        string[] GAME = new string[9];
+        int playerturn = 0;
+
+        public string returnsym(int turn)
+        {
+            if (turn % 2 == 0)
+            {
+                return "X";
+            }
+            else
+            {
+                return "O";
+            }
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[0] = returnsym(playerturn);
+            btn1.Text = GAME[0];
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[1] = returnsym(playerturn);
+            btn2.Text = GAME[1];
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[2] = returnsym(playerturn);
+            btn3.Text = GAME[2];
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[3] = returnsym(playerturn);
+            btn4.Text = GAME[3];
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[4] = returnsym(playerturn);
+            btn5.Text = GAME[4];
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[5] = returnsym(playerturn);
+            btn6.Text = GAME[5];
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[6] = returnsym(playerturn);
+            btn7.Text = GAME[6];
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[7] = returnsym(playerturn);
+            btn8.Text = GAME[7];
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            playerturn++;
+            GAME[8] = returnsym(playerturn);
+            btn9.Text = GAME[8];
+        }
     }
 }
